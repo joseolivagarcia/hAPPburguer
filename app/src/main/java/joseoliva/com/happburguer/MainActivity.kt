627bb23ba8fd204c14664f911cadf79b4960089c
Jose Oliva Local <jose@joseoliva.com>
2022-11-02 10:02:35 +0100

    Preparada actividad principal con su toolbar y viewpager2

--- app/src/main/java/joseoliva/com/happburguer/MainActivity.kt
+++ app/src/main/java/joseoliva/com/happburguer/MainActivity.kt
@@ -1,7 +1,13 @@
 package joseoliva.com.happburguer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.Toolbar
 import joseoliva.com.happburguer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +18,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //inicio mi toolbar y sobreescribo los metodos necesarios (al final)
+        val toolbar: Toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+
+    }
+
+    //para poner menus en el toolbar sobreescribo estos dos metodos
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mimenu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    //para indicar una accion al pulsar un item del menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home -> {
+                Toast.makeText(this,"home",Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.carrito -> {
+                Toast.makeText(this,"carrito",Toast.LENGTH_SHORT).show()
+                return  true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
